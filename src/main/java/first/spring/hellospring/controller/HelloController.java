c9a71211b3d97811afb218810cb957c5e0c6ac46
ohyuchan <oyuchan50@gmail.com>
2022-04-12 23:29:41 +0900

    API

--- src/main/java/first/spring/hellospring/controller/HelloController.java
+++ src/main/java/first/spring/hellospring/controller/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 
@@ -15,9 +17,35 @@ public class HelloController {
         return "hello";
     }
     @GetMapping("hello-mvc")
-    public String helloMvc(@RequestAttribute("name")String name, Model model)
-    {
+    public String helloMvc(@RequestParam("name")String name, Model model) {
         model.addAttribute("name",name);
         return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody //http에서 body부분에 이 데이터를 직접 넣어주겠다는 말
+    public String helloString(@RequestParam("name")String name)
+    {
+        return "hello "+name;//hello spring
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name")String name)
+    {
+        Hello hello=new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
